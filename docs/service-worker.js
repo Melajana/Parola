f625aaaad84abf716aadb1ce01e8272a9173d1fb
melanie <63152122+Melajana@users.noreply.github.com>
2025-08-08 10:32:14 +0200

    Add files via upload

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,11 +1,13 @@
-// Simple offline cache
-const CACHE = 'it-vokabeltrainer-v1';
+// cache v2 for style changes
+const CACHE = 'parola-cache-v2';
 const ASSETS = ['.', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request).then(r => {
